efc15a8385382cb3c916a822b1f01d9360cfc35c
Adam Belson <ad@mbelson.com>
2014-10-29 17:39:27 +0000

    http server code

---
+++ Static_http_server/http_client_static.js
@@ -0,0 +1,26 @@
+var http = require('http');
+
+var options = {
+    hostname: 'localhost',
+    port: '8088',
+    path: '/index.html'
+  };
+  
+function handleResponse(response) {
+  
+  var serverData = '';
+  
+  response.on('data', function (chunk) {
+    serverData += chunk;
+  });
+  
+  response.on('end', function () {
+    console.log(serverData);
+  });
+  
+}
+http.request(options, function(response){
+
+  handleResponse(response);
+  
+}).end();
